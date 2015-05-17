83ee70f38be7bf0097aae7fba5cefe90e65ad71e
NikolayU <nikolaiuhvarov@mail.ru>
2015-05-17 22:51:00 +0300

    first

---
+++ TestTask.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace Testovoe
+{
+    partial class TestTask
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DictionaryPath = new System.Windows.Forms.TextBox();
+            this.TextPath = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // DictionaryPath
+            // 
+            this.DictionaryPath.Enabled = false;
+            this.DictionaryPath.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.DictionaryPath.Location = new System.Drawing.Point(31, 34);
+            this.DictionaryPath.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.DictionaryPath.Name = "DictionaryPath";
+            this.DictionaryPath.Size = new System.Drawing.Size(355, 22);
+            this.DictionaryPath.TabIndex = 0;
+            // 
+            // TextPath
+            // 
+            this.TextPath.Enabled = false;
+            this.TextPath.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.TextPath.Location = new System.Drawing.Point(31, 98);
+            this.TextPath.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.TextPath.Name = "TextPath";
+            this.TextPath.Size = new System.Drawing.Size(355, 22);
+            this.TextPath.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.PowderBlue;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Location = new System.Drawing.Point(394, 32);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(49, 28);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.PowderBlue;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Location = new System.Drawing.Point(394, 96);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(49, 28);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "...";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.PowderBlue;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.Location = new System.Drawing.Point(31, 153);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(128, 42);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Создать HTML файл(ы)";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(493, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(196, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Указать путь файла словаря";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(493, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(207, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Указать путь файла с текстом";
+            // 
+            // TestTask
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Azure;
+            this.ClientSize = new System.Drawing.Size(709, 230);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.TextPath);
+            this.Controls.Add(this.DictionaryPath);
+            this.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "TestTask";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TestTask";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox DictionaryPath;
+        private System.Windows.Forms.TextBox TextPath;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ TestTask.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Testovoe
+{
+    public partial class TestTask : Form
+    {
+        public TestTask()
+        {
+            InitializeComponent();
+        }
+
+        OpenFileDialog openFileDialog1 = new OpenFileDialog();
+        OpenFileDialog openFileDialog2 = new OpenFileDialog();
+        string[] dictionary;  // словарь
+        int n ;  // счетчик созданных файлов (0 не пишется)
+        const int N = 20;  // количество строк в одном файле
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.InitialDirectory = "C://";
+            openFileDialog1.Filter = "text files (*.txt,*.fb2,*.doc,*.docx,*.rtf,*.odt)|*.txt;*.fb2;*.doc;*.docx;*.rtf;*.odt";
+            openFileDialog1.FilterIndex = 1;
+            openFileDialog1.RestoreDirectory = true;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    StreamReader file = File.OpenText(openFileDialog1.FileName);
+                    dictionary = file.ReadToEnd().Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+                    DictionaryPath.Text = openFileDialog1.FileName;
+                    // Проверка условия "Файл словаря содержит произвольное количество строк, каждая из которых содержит ровно одно слово":
+                    for (int i = 0; i < dictionary.Length; i++)
+                    {
+                        string[] temp;
+                        temp = dictionary[i].Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                        if (temp.Length != 1)
+                        {
+                            MessageBox.Show("Неверная структура файла словаря");
+                            DictionaryPath.Text = "";
+                            break;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ошибка при чтении файла: " + ex.Message);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            openFileDialog2.InitialDirectory = "C://";
+            openFileDialog2.Filter = "text files (*.txt,*.fb2,*.doc,*.docx,*.rtf,*.odt)|*.txt;*.fb2;*.doc;*.docx;*.rtf;*.odt";
+            openFileDialog2.FilterIndex = 1;
+            openFileDialog2.RestoreDirectory = true;
+            if (openFileDialog2.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    StreamReader file = File.OpenText(openFileDialog2.FileName);
+                    TextPath.Text = openFileDialog2.FileName;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ошибка при чтении файла: " + ex.Message);
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (DictionaryPath.Text != "" && TextPath.Text != "")
+            {
+                string[] slova = new string[1000];  // массив слов строки файла с текстом
+                char[] simvol;  
+                int stroka = 0;
+                StreamWriter sw = new StreamWriter("C:/HTMLFile.html", true, System.Text.Encoding.Default);
+                using (StreamReader sr = new StreamReader(openFileDialog2.FileName, System.Text.Encoding.Default))
+                {
+                    bool eq = false;  
+                    sw.WriteLine("<html>");
+                    // Отделение знаков препинания:
+                    while (sr.Peek() >= 0)
+                    {
+                        Array.Clear(slova, 0, slova.Length);
+                        simvol = sr.ReadLine().ToCharArray();
+                        int k = 0;
+                        for (int l = 0; l < simvol.Length; l++)
+                        {
+                            if (simvol[l] != ' ' && simvol[l] != ',' && simvol[l] != '.' && simvol[l] != '!' && simvol[l] != '?')
+                            {
+                                slova[k] += simvol[l];
+                            }
+                            else
+                            {
+                                slova[k + 1] += simvol[l];
+                                k += 2;
+                            }
+                        }
+                        // Поиск совпадающих слов и запись:
+                        for (int i = 0; i < slova.Length; i++)
+                        {
+                            for (int j = 0; j < dictionary.Length; j++)
+                            {
+                                if (slova[i] != null)
+                                    if (slova[i].ToLower() == dictionary[j].ToLower())
+                                    {
+                                        eq = true;
+                                    }
+                            }
+                            if (eq)
+                            {
+                                sw.Write("<b><i>" + slova[i] + "</b></i>");
+                            }
+                            else
+                            {
+                                sw.Write(slova[i]);
+                            }
+                            eq = false;
+                        }
+                        sw.Write("<br>"); 
+                        stroka++;
+                        sw.WriteLine();
+                        if (stroka >= N)  // Проверка условия "Если файл превышает размер N строк, то его необходимо разбить на несколько файлов, каждый не более N строк"
+                        {
+                            sw.WriteLine("</html>");
+                            sw.Close();
+                            n++;
+                            stroka = 0;
+                            sw = new StreamWriter("C:/HTMLFile" + n.ToString() + ".html", true, System.Text.Encoding.Default);
+                            sw.WriteLine("<html>");
+                        }
+                    }
+                    sw.WriteLine("</html>");
+                    sw.Close();
+                }
+            }
+            else
+                MessageBox.Show("Указан пустой путь");
+        }
+    }
+}
